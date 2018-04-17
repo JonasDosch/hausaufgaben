df3460fd1a39a61890e119ad5a08050afd8d37b1
JonasDosch <36501946+JonasDosch@users.noreply.github.com>
2018-04-17 10:38:54 +0200

    a

--- hausaufgaben/src/hausaufgaben/ShapesReadTest.java
+++ hausaufgaben/src/hausaufgaben/ShapesReadTest.java
@@ -1,40 +1,57 @@
 package hausaufgaben;
 import static jsTools.Input.*;
+import static jsTools.Graph.*;
 
 
 
 public class ShapesReadTest {
 
+
+
 	public static void main(String[] args) {
 		double PI = Math.PI;
-		
-		java.util.Scanner scanner = new java.util.Scanner(System.in);
+		int x = 10;
+		int y = 10;
+
+/*		java.util.Scanner scanner = new java.util.Scanner(System.in);
 		double p = Double.parseDouble(scanner.nextLine());
+		*/
 		
 		for(int w = 1; w < 2; w++) {
 			
 			w--;
 			
-		String a = readString("Geben Sie für ein Rechteck ´R´ ein und für einen Kreis ´C´ an!?");
+		String a = readString("Geben Sie für ein Rechteck ´R´ ein und für einen Kreis ´C´ an!? Wenn Sie keinen Bock mehr haben geben Sie ´x´ an!");
 		
 		if(a.equals("R")){
-			double i = readInteger("Welche Höhe hat das Rechteck?");
-			double j = readInteger("Welche Breite hat das Rechteck?");
+			double i = readDouble("Welche Höhe hat das Rechteck?");
+			double j = readDouble("Welche Breite hat das Rechteck?");
 			
 			System.out.println("Die Fläche Ihres Rechtecks ist: " + i*j);
 			
-		//	addRect(10,10,j,i, );
+			int g = (int) i;
+			int f = (int) j;
+				
+			addRect(x,y,f,g,blue);
 			
 			
 		}
 		else if(a.equals("C")) {
 	
-			int r = readInteger("Welchen Radius soll der Kreis haben?");
+			double r = readDouble("Welchen Radius soll der Kreis haben?");
 			
 			System.out.println("Die Fläche Ihres Kreises ist: " + r*r*PI);
 			
-		//	addCircle(10,10,2*r,);
+			int ra = (int) r;
 			
+			addCircle(x,y,2*ra,yellow);
+			
+		}
+		
+		else if(a.equals("x")) {
+			
+			System.out.println("Vielen Dank, dass Du mich aus der Endlosschleife befreit hast!");
+			break;
 		}
 		
 		else{
