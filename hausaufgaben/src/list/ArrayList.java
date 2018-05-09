7afd41c1a83350d152ee213f55b63fd8c66beb65
JonasDosch <36501946+JonasDosch@users.noreply.github.com>
2018-05-09 16:29:33 +0200

    a

--- hausaufgaben/src/list/ArrayList.java
+++ hausaufgaben/src/list/ArrayList.java
@@ -1,5 +1,43 @@
 package list;
 
-public class ArrayList {
+
+public class ArrayList<E> implements List{
+
+	E[] myArray;
+	
+	public ArrayList(int capacity){
+		E[] myArray = (E[]) new Object[capacity];
+			}
+	
+	@Override
+	public void add(Object element) {
+		for(int i = 0; i < myArray.length ; i++) {
+			if(myArray[i] == null) {
+				myArray[i] = (E) element;
+			}
+		}
+		
+	}
+
+	@Override
+	public Object get(int index) {
+	return myArray[index];
+	
+	}
+
+	@Override
+	public void set(int index, Object element) {
+		
+		myArray[index] = (E) element;
+	}
+
+	@Override
+	public int size() {
+		int size=0;
+		for(int i = 0; i < myArray.length; i++) {
+			size = size +1;
+		}
+		return size;
+	}
 
 }
--- hausaufgaben/src/list/List.java
+++ hausaufgaben/src/list/List.java
@@ -1,5 +1,13 @@
 package list;
 
-public interface List {
+public interface List<E> {
+	
+	void add(E element);
+	
+	E get(int index);
+	
+	void set (int index, E element);
+	
+	int size();
 
 }
